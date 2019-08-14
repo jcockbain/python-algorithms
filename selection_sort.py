24daf15b41dbf565aec9a2dc629ee22c2ac959c8
James Cockbain <james.cockbain@ibm.com>
2019-08-14 17:28:39 +0100

    add selection sort

---
+++ selection_sort.py
@@ -0,0 +1,13 @@
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+arr = [64, 25, 34, 75, 23]
+selection_sort(arr)
+
+print(arr)
