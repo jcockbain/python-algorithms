9da936d8eb7dafdad91a580c94f298e17a15499f
James Cockbain <james.cockbain@ibm.com>
2020-04-01 22:54:06 +0100

    add linked list

---
+++ linkedlist/test_linkedlist.py
@@ -0,0 +1,15 @@
+import unittest
+
+from linkedlist import Node
+
+class TestSuite(unittest.TestCase):
+    def test_node(self):
+        head = Node(0)
+        head.next = Node(1)
+        head.next.next = Node(2)
+
+        self.assertEqual(head.next.next.val, 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
