4da17b2850489957eb3494c6e18201312e7b0e7d
James Cockbain <james.cockbain@ibm.com>
2020-04-04 22:19:05 +0100

    increase coverage

--- sorting/test_sorting.py
+++ sorting/test_sorting.py
@@ -5,9 +5,27 @@ from selection_sort import selection_sort
 
 
 class TestSuite(unittest.TestCase):
-    def test_sort(self):
+    def test_sort_1(self):
         arr = [5, 1, 2, 3, 4]
         expected = [1, 2, 3, 4, 5]
+        self.assertEqual(expected, insertion_sort(arr))
+        self.assertEqual(expected, selection_sort(arr))
+
+    def test_sort_2(self):
+        arr = [5, 2, 2, 3, 4, 5]
+        expected = [2, 2, 3, 4, 5, 5]
+        self.assertEqual(expected, insertion_sort(arr))
+        self.assertEqual(expected, selection_sort(arr))
+
+    def test_sort_3(self):
+        arr = [5, 4, -3, 2, 1, 0]
+        expected = [-3, 0, 1, 2, 4, 5]
+        self.assertEqual(expected, insertion_sort(arr))
+        self.assertEqual(expected, selection_sort(arr))
+
+    def test_sort_4(self):
+        arr = [1] * 5
+        expected = [1] * 5
         self.assertEqual(expected, insertion_sort(arr))
         self.assertEqual(expected, selection_sort(arr))
 
