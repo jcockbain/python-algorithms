9a86dc9563d3b387981538053f777d9e8820736c
James Cockbain <james.cockbain@ibm.com>
2020-05-02 14:06:24 +0100

    add extra sorting test case

--- sorting/test_sorting.py
+++ sorting/test_sorting.py
@@ -28,3 +28,9 @@ class TestSuite(unittest.TestCase):
         expected = [1] * 5
         self.assertEqual(expected, insertion_sort(arr))
         self.assertEqual(expected, selection_sort(arr))
+
+    def test_sort_5(self):
+        arr = [10, 1, 4, 3, 2, 11, 14, 13, 12, 15, 7, 6, 8, 9, 5]
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        self.assertEqual(expected, insertion_sort(arr))
+        self.assertEqual(expected, selection_sort(arr))
